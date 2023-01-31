fdf903ef2b2d7b12b6de57fab28874b280a9f00b
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-01-31 11:54:51 +0000

    fix date command

--- get-all-renovate-prs-helm.sh
+++ get-all-renovate-prs-helm.sh
@@ -9,7 +9,7 @@ platform=$(uname)
 
 if [ $platform == "Darwin" ]; then
 
-    packages=(gawk gsed yj jq)
+    packages=(coreutils gawk gsed yj jq)
 
     for i in "${packages[@]}"
 
@@ -21,6 +21,7 @@ if [ $platform == "Darwin" ]; then
         else
             awk_command=$(which gawk)
             sed_command=$(which gsed)
+            date_command=$(which gdate)
     fi
     done
 
@@ -38,13 +39,14 @@ elif [ $platform == "Linux" ]; then
         else
             awk_command=$(which awk)
             sed_command=$(which sed)
+            date_command=$(which date)
     fi
     done
 fi
 
 slack_bot_token=$SLACK_BOT_TOKEN
 
-repos=$(gh search prs "Update Helm release" --owner hmcts --author app/renovate --state=open --created="*..$(date -v-7d  +"%Y"-"%m"-"%d")" --sort=created --json repository | jq -r '. | unique_by(.repository.name)' | jq -r '.[].repository.name')
+repos=$(gh search prs "Update Helm release" --owner hmcts --author app/renovate --state=open --created="*..$($date_command --date="7 days ago" +"%Y"-"%m"-"%d")" --sort=created --json repository | jq -r '. | unique_by(.repository.name)' | jq -r '.[].repository.name')
 
 for repo in $(echo "${repos[@]}"); do
     
@@ -68,7 +70,7 @@ for repo in $(echo "${repos[@]}"); do
         # contact_channel="#platform-operations"
         message_data="{\"channel\":\"$contact_channel\",\"blocks\": [{\"type\": \"section\",\"text\": {\"type\": \"mrkdwn\",\"text\": \"*Helm dependencies are out of date - <https://github.com/hmcts/$repo|$repo>*\"}},{\"type\": \"section\",\"text\": {\"type\": \"mrkdwn\",\"text\": \"There are renovate pull requests pending review to update helm charts in $repo.\"}},{\"type\": \"section\",\"text\": {\"type\": \"mrkdwn\",\"text\": \"These are needed to keep your app up to date so please review the pull requests at your earliest convenience.\"}},{\"type\": \"actions\",\"elements\": [{\"type\": \"button\",\"text\": {\"type\": \"plain_text\",\"text\": \"Click here to view pending PRs\",\"emoji\": true},\"url\": \"https://github.com/hmcts/$repo/pulls?q=is%3Apr+is%3Aopen+sort%3Aupdated-desc+author%3Aapp%2Frenovate+Helm+in%3Atitle\"}]}]}"
     else
-        contact_channel="#slack_msg_format_testing"
+        contact_channel="#test-ek-2"
         message_data="{\"channel\":\"$contact_channel\",\"blocks\": [{\"type\": \"section\",\"text\": {\"type\": \"mrkdwn\",\"text\": \"*Helm dependencies are out of date - <https://github.com/hmcts/$repo|$repo>*\"}},{\"type\": \"section\",\"text\": {\"type\": \"mrkdwn\",\"text\": \"There are renovate pull requests pending review to update helm charts in $repo.\"}},{\"type\": \"section\",\"text\": {\"type\": \"mrkdwn\",\"text\": \"These are needed to keep your app up to date so please review the pull requests at your earliest convenience.\"}},{\"type\": \"actions\",\"elements\": [{\"type\": \"button\",\"text\": {\"type\": \"plain_text\",\"text\": \"Click here to view pending PRs\",\"emoji\": true},\"url\": \"https://github.com/hmcts/$repo/pulls?q=is%3Apr+is%3Aopen+sort%3Aupdated-desc+author%3Aapp%2Frenovate+Helm+in%3Atitle\"}]}]}"
     fi
 
@@ -80,7 +82,7 @@ for repo in $(echo "${repos[@]}"); do
 
     if [ $(echo $post_request | jq '.ok') = "false" ]; then
         reason=$(echo $post_request | jq -r '.error')
-        message_data="{\"channel\": \"#slack_msg_format_testing\",\"blocks\": [{\"type\": \"section\",\"text\": {\"type\": \"mrkdwn\",\"text\": \"*Message could not be sent - <https://github.com/hmcts/$repo|$repo>*\"}},{\"type\": \"section\",\"text\": {\"type\": \"mrkdwn\",\"text\": \"A message regarding renovate helm dependencies could not be sent to *$contact_channel*\"}},{\"type\": \"section\",\"text\": {\"type\": \"mrkdwn\",\"text\": \"The error message was: *$reason*\"}},{\"type\": \"section\",\"text\": {\"type\": \"mrkdwn\",\"text\": \":warning: *Please investigate this error and attempt to fix so messages get sent to the correct channel*\"}},{\"type\": \"actions\",\"elements\": [{\"type\": \"button\",\"text\": {\"type\": \"plain_text\",\"text\": \"Click here to view pending PRs\",\"emoji\": true},\"url\": \"https://github.com/hmcts/$repo/pulls?q=is%3Apr+is%3Aopen+sort%3Aupdated-desc+author%3Aapp%2Frenovate+Helm+in%3Atitle\"}]}]}"
+        message_data="{\"channel\": \"#test-ek-2\",\"blocks\": [{\"type\": \"section\",\"text\": {\"type\": \"mrkdwn\",\"text\": \"*Message could not be sent - <https://github.com/hmcts/$repo|$repo>*\"}},{\"type\": \"section\",\"text\": {\"type\": \"mrkdwn\",\"text\": \"A message regarding renovate helm dependencies could not be sent to *$contact_channel*\"}},{\"type\": \"section\",\"text\": {\"type\": \"mrkdwn\",\"text\": \"The error message was: *$reason*\"}},{\"type\": \"section\",\"text\": {\"type\": \"mrkdwn\",\"text\": \":warning: *Please investigate this error and attempt to fix so messages get sent to the correct channel*\"}},{\"type\": \"actions\",\"elements\": [{\"type\": \"button\",\"text\": {\"type\": \"plain_text\",\"text\": \"Click here to view pending PRs\",\"emoji\": true},\"url\": \"https://github.com/hmcts/$repo/pulls?q=is%3Apr+is%3Aopen+sort%3Aupdated-desc+author%3Aapp%2Frenovate+Helm+in%3Atitle\"}]}]}"
         curl -s -H "Content-type: application/json" --data "$message_data" -H "Authorization: Bearer ${slack_bot_token}" -H application/json -X POST https://slack.com/api/chat.postMessage
     fi
 
