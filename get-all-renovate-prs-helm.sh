f0357cbb9e633b0966429bb63cd7c65574ffecd8
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-02-01 11:29:00 +0000

    add info to script

--- get-all-renovate-prs-helm.sh
+++ get-all-renovate-prs-helm.sh
@@ -46,6 +46,7 @@ fi
 
 slack_bot_token=$SLACK_BOT_TOKEN
 
+# get all prs in organisation that have a specific title and created by renovate over a week ago
 repos=$(gh search prs "Update Helm release" --owner hmcts --author app/renovate --state=open --created="*..$($date_command --date="7 days ago" +"%Y"-"%m"-"%d")" --sort=created --json repository | jq -r '. | unique_by(.repository.name)' | jq -r '.[].repository.name')
 
 for repo in $(echo "${repos[@]}"); do
@@ -58,6 +59,7 @@ for repo in $(echo "${repos[@]}"); do
         contact_channel="#test-ek-3"
         # contact_channel="#platform-operations"
     else
+        # extract slack contact channel from file
         contact_channel=$(curl -s https://raw.githubusercontent.com/hmcts/cnp-jenkins-config/master/team-config.yml | yq $(echo $slackChannel | tr -d '"'))
     fi
 
@@ -74,12 +76,14 @@ for repo in $(echo "${repos[@]}"); do
         message_data="{\"channel\":\"$contact_channel\",\"blocks\": [{\"type\": \"section\",\"text\": {\"type\": \"mrkdwn\",\"text\": \"*Helm dependencies are out of date - <https://github.com/hmcts/$repo|$repo>*\"}},{\"type\": \"section\",\"text\": {\"type\": \"mrkdwn\",\"text\": \"There are renovate pull requests pending review to update helm charts in $repo.\"}},{\"type\": \"section\",\"text\": {\"type\": \"mrkdwn\",\"text\": \"These are needed to keep your app up to date so please review the pull requests at your earliest convenience.\"}},{\"type\": \"actions\",\"elements\": [{\"type\": \"button\",\"text\": {\"type\": \"plain_text\",\"text\": \"Click here to view pending PRs\",\"emoji\": true},\"url\": \"https://github.com/hmcts/$repo/pulls?q=is%3Apr+is%3Aopen+sort%3Aupdated-desc+author%3Aapp%2Frenovate+Helm+in%3Atitle\"}]}]}"
     fi
 
+    # send slack message
     post_request=$(curl -s -H "Content-type: application/json" \
     --data "$message_data" \
     -H "Authorization: Bearer ${slack_bot_token}" \
     -H application/json \
     -X POST https://slack.com/api/chat.postMessage)
 
+    # send message when slack channel cannot be found or cannot be posted to
     if [ $(echo $post_request | jq '.ok') = "false" ]; then
         reason=$(echo $post_request | jq -r '.error')
         message_data="{\"channel\": \"#slack_msg_format_testing\",\"blocks\": [{\"type\": \"section\",\"text\": {\"type\": \"mrkdwn\",\"text\": \"*Message could not be sent - <https://github.com/hmcts/$repo|$repo>*\"}},{\"type\": \"section\",\"text\": {\"type\": \"mrkdwn\",\"text\": \"A message regarding renovate helm dependencies could not be sent to *$contact_channel*\"}},{\"type\": \"section\",\"text\": {\"type\": \"mrkdwn\",\"text\": \"The error message was: *$reason*\"}},{\"type\": \"section\",\"text\": {\"type\": \"mrkdwn\",\"text\": \":warning: *Please investigate this error and attempt to fix so messages get sent to the correct channel*\"}},{\"type\": \"actions\",\"elements\": [{\"type\": \"button\",\"text\": {\"type\": \"plain_text\",\"text\": \"Click here to view pending PRs\",\"emoji\": true},\"url\": \"https://github.com/hmcts/$repo/pulls?q=is%3Apr+is%3Aopen+sort%3Aupdated-desc+author%3Aapp%2Frenovate+Helm+in%3Atitle\"}]}]}"
