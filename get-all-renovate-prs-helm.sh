7e149a2579d70fd45ac119424cb08ce8a2a7d961
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-02-10 10:25:21 +0000

    uncommment

--- get-all-renovate-prs-helm.sh
+++ get-all-renovate-prs-helm.sh
@@ -90,11 +90,11 @@ for repo in $(echo "${repos[@]}"); do
     message_data="{\"channel\": \"#dependencies-helm\",\"blocks\": [{\"type\": \"section\",\"text\": {\"type\": \"mrkdwn\",\"text\": \"*Helm dependencies are out of date - <https://github.com/hmcts/$repo|$repo>*\"}},{\"type\": \"section\",\"text\": {\"type\": \"mrkdwn\",\"text\": \"The following updates are pending in $repo:\n\n$updates\"}},{\"type\": \"section\",\"text\": {\"type\": \"mrkdwn\",\"text\": \"You can contact the team that owns this repo via the *$contact_channel* channel on slack.\"}},{\"type\": \"section\",\"text\": {\"type\": \"mrkdwn\",\"text\": \"These are needed to keep your app up to date so please review the pull requests at your earliest convenience.\"}},{\"type\": \"actions\",\"elements\": [{\"type\": \"button\",\"text\": {\"type\": \"plain_text\",\"text\": \"Click here to view all PRs\",\"emoji\": true},\"url\": \"https://github.com/hmcts/$repo/pulls?q=is%3Apr+is%3Aopen+sort%3Aupdated-desc+author%3Aapp%2Frenovate+Helm+in%3Atitle\"}]}]}"
 
     # send slack message
-    # curl -s -H "Content-type: application/json" \
-    # --data "$message_data" \
-    # -H "Authorization: Bearer ${slack_bot_token}" \
-    # -H application/json \
-    # -X POST https://slack.com/api/chat.postMessage
+    curl -s -H "Content-type: application/json" \
+    --data "$message_data" \
+    -H "Authorization: Bearer ${slack_bot_token}" \
+    -H application/json \
+    -X POST https://slack.com/api/chat.postMessage
 
     echo "contact channel is $contact_channel"
 
